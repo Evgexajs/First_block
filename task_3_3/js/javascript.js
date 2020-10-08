206abc2421549f9b8ed297089ddff24b917b47c9
DESKTOP-VHE2EHL\Евгений <ekopytov509@gmail.com>
2020-10-08 21:57:43 +0500

    task_3_3

---
+++ task_3_3/js/javascript.js
@@ -0,0 +1,35 @@
+
+var slideIndex = 1;
+function onLoad() {
+  showSlides(slideIndex);
+  intervalId = window.setInterval(function () { showSlides(slideIndex += 1); }, 3000);
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}  
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshow__slide");
+  var dots = document.getElementsByClassName("slideshow__dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function modals() {
+  document.getElementById("modal-img").style.display = "block";
+  document.getElementById("modal-img__content").src = document.getElementsByClassName("image")[slideIndex-1].src;
+}
